8cb485eb6354465fc15a416b42aeb304aa8247a9
Alyx Rossetti <arossetti@getty.edu>
2015-05-21 15:30:25 -0700

    changed ip back to localhost for the time being

--- app/js/services.js
+++ app/js/services.js
@@ -6,7 +6,7 @@ var portalServices = angular.module('portalServices', ['elasticsearch']);
 
 portalServices.service('ESclient', function(esFactory) {
   return esFactory({
-    host: 'http://192.168.59.103:9200',
+    host: 'http://localhost:9200',
     apiVersion: '1.3',
     log: 'trace'
   });
