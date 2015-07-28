d8e4d95f6a1a0a769a1ceb8299e4ba60f74cf40f
Susan Ley <SLey@getty.edu>
2015-07-28 14:25:19 -0700

    FAQs page complete

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -81,4 +81,31 @@ portalControllers.controller('FooterCtrl', ['$scope', '$routeParams',
 portalControllers.controller('FaqsCtrl', ['$scope', '$routeParams',
   function ($scope) {
     $scope.faqs = {name: "faqs.html", url: "faqs.html"};
+    $scope.activeTabs = [];
+
+    // check if the tab is active
+    $scope.isOpenTab = function (tab) {
+        // check if this tab is already in the activeTabs array
+        if ($scope.activeTabs.indexOf(tab) > -1) {
+            // if so, return true
+            return true;
+        } else {
+            // if not, return false
+            return false;
+        }
+    };
+
+    // function to 'open' a tab
+    $scope.openTab = function (tab) {
+        // check if tab is already open
+        if ($scope.isOpenTab(tab)) {
+            //if it is, remove it from the activeTabs array
+            $scope.activeTabs.splice($scope.activeTabs.indexOf(tab), 1);
+            
+        } else {
+            // if it's not, add it!
+            $scope.activeTabs.push(tab);
+        }
+    };
+
 }]);
