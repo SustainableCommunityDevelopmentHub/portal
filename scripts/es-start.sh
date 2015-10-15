6321a81000835b6ba2788d1d750409c78566789d
adamcee <adam.cahan@gmail.com>
2015-10-14 18:05:24 -0700

    cleanup

--- scripts/es-start.sh
+++ scripts/es-start.sh
@@ -3,28 +3,35 @@
 #10/9/15
 #Start elasticsearch docker container
 
-#Behavior:
-#Don't continue script if container already running...
-#Check if container named 'elasticsearch' (or whatever ES_CONTAINER_NAME is set to) exists.
-#If a container named 'elasticsearch' does exist, assume its our container and start it.
-#If not, assume ES container has not been created from image.
-#Go ahead and create ES container with proper settings and assign name.
+#NOTE: This script assumes it is being run from project root (for mounting docker data volumes, for es config
+#      file) and will break otherwise.
 
 ES_CONTAINER_NAME=elasticsearch
 ES_CONTAINER_ID=$(docker ps -aq -f name=$ES_CONTAINER_NAME)
 
+
+#Don't continue script if container already running...
 if [ -z $(docker ps -q -f name=$ES_CONTAINER_NAME) ];
 then
+    #Check if container named 'elasticsearch' (or whatever ES_CONTAINER_NAME is set to) exists.
     if [ $ES_CONTAINER_ID ]; then
+        #If a container named 'elasticsearch' does exist, assume its our container and start it.
         echo "Elasticsearch docker container exists. Starting container...."
         docker start $ES_CONTAINER_NAME;
         echo $ES_CONTAINER_ID
     else
+        #If not, assume ES container has not been created from image.
         echo "Creating and starting elasticsearch docker container...."
         echo $ES_CONTAINER_ID
 
-        #Use ES's JAVA_OPTS (-Des) arg to enable CORS -- NOTE: REVIEW BEFORE GOING INTO PRODUCTION
-        docker run -dit -p 9200:9200 -p 9300:9300 --name $ES_CONTAINER_NAME elasticsearch:1.7 -Des.http.cors.enabled=true
+        #Go ahead and create ES container with proper settings and assign name. Name needed for script to work.
+        docker run -dit -p 9200:9200 -p 9300:9300 --name $ES_CONTAINER_NAME elasticsearch:1.7
+
+        #Set volume for ES container for elasticsearch.yml config file -- Work in progress
+        #docker run -dit -p 9200:9200 -p 9300:9300 -v $(pwd)/mount/elastic_config:/user/share/elasticsearch/config --name $ES_CONTAINER_NAME elasticsearch:1.7
+
+        #Enable CORS with command line arg -- Work in progress
+        docker run -it -p 9200:9200 -p 9300:9300 --name $ES_CONTAINER_NAME elasticsearch:1.7 -Des.http.cors.enabled=true
     fi
 fi
 
