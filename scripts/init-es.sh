76abc4aad9aa9f32352db8a8246b614b4fcad059
adamcee <adam.cahan@gmail.com>
2015-10-23 11:35:14 -0700

    Fix error in init-es.sh. Sudo statement should be in npm script which calls shell script. Also shell script missing double quote

--- scripts/init-es.sh
+++ scripts/init-es.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Initialize ES inside the docker container: make index, add sample data, etc, start ES.
 
-echo "Restarting elasticsearch...
-sudo /etc/init.d/elasticsearch restart
-echo "Waiting for elasticsearch to get itself together....
+echo "Starting elasticsearch..."
+/etc/init.d/elasticsearch start
+echo "Waiting for elasticsearch to get itself together...."
 sleep 10
 echo "Creating portal index..."
 curl -XPUT http://localhost:9200/portal
