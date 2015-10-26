00e2e99fe8e38622d745056bd7aa1622841f4e65
adamcee <adam.cahan@gmail.com>
2015-10-26 14:04:53 -0700

    Wrap animation.js in anonymous function. Fix small err in app.js'

--- app/js/animations.js
+++ app/js/animations.js
@@ -1,52 +1,55 @@
-var portalAnimations = angular.module('portalAnimations', ['ngAnimate']);
+(function() {
+  'use strict';
+  angular.module('portalAnimations', ['ngAnimate'])
 
-portalAnimations.animation('.book', function() {
+  .animation('.book', function() {
 
-  var animateUp = function(element, className, done) {
-    if(className != 'active') {
-      return;
-    }
-    element.css({
-      position: 'absolute',
-      top: 500,
-      left: 0,
-      display: 'block'
-    });
-
-    jQuery(element).animate({
-      top: 0
-    }, done);
-
-    return function(cancel) {
-      if(cancel) {
-        element.stop();
+    var animateUp = function(element, className, done) {
+      if(className != 'active') {
+        return;
       }
-    };
-  }
+      element.css({
+        position: 'absolute',
+        top: 500,
+        left: 0,
+        display: 'block'
+      });
 
-  var animateDown = function(element, className, done) {
-    if(className != 'active') {
-      return;
+      jQuery(element).animate({
+        top: 0
+      }, done);
+
+      return function(cancel) {
+        if(cancel) {
+          element.stop();
+        }
+      };
     }
-    element.css({
-      position: 'absolute',
-      left: 0,
-      top: 0
-    });
 
-    jQuery(element).animate({
-      top: -500
-    }, done);
-
-    return function(cancel) {
-      if(cancel) {
-        element.stop();
+    var animateDown = function(element, className, done) {
+      if(className != 'active') {
+        return;
       }
-    };
-  }
+      element.css({
+        position: 'absolute',
+        left: 0,
+        top: 0
+      });
 
-  return {
-    addClass: animateUp,
-    removeClass: animateDown
-  };
-});
+      jQuery(element).animate({
+        top: -500
+      }, done);
+
+      return function(cancel) {
+        if(cancel) {
+          element.stop();
+        }
+      };
+    }
+
+    return {
+      addClass: animateUp,
+      removeClass: animateDown
+    };
+  });
+})();
