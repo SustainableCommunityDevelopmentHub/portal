6e1a2ebc4b537f54d315f07e61c775034bfde667
adamcee <adam.cahan@gmail.com>
2015-11-16 19:43:37 -0800

    Broke app routing out into separate file

---
+++ app/app.config.routing.js
@@ -0,0 +1,91 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+      // Redirect to home by default
+      $urlRouterProvider.otherwise('/');
+
+      // Assign states to urls
+      $stateProvider
+        // Abstract state used to load other search states
+        .state('home', {
+          url: '/',
+          templateUrl: 'search/search.home.html',
+          controller: 'HomePageCtrl'
+        })
+
+        .state('searchResults', {
+          url: '/search?q',
+          controller: 'SearchCtrl',
+          templateUrl: 'search/search.results.html',
+          resolve: {
+             //Run search and load resulting promise into controller prior to state load
+            result: function($stateParams, SearchService){
+              return SearchService.search({q: $stateParams.q});
+            }
+          }
+        })
+
+        .state('ProtosearchResults', {
+          url: '/protosearch',
+          controller: 'SearchCtrl',
+          //templateUrl: 'partials/search.results.old.html'
+          views: {
+            // Main template for searchResults
+            '': { templateUrl: 'search/search.results.html'
+            },
+
+            // Child views for searchResults
+            'facets@searchResults': {
+              templateUrl: 'search/search.facets.html'
+            },
+            'data@searchResults': {
+              templateUrl: 'search/search.data.html'
+            }
+          }
+        })
+
+        .state('books', {
+          url: '/books/:bookID',
+          templateUrl: 'partials/book-detail.html',
+          controller: 'BookDetailCtrl'
+        })
+
+        .state('advanced', {
+          url: '/advanced',
+          templateUrl: 'partials/advanced.html',
+          controller: 'AdvancedCtrl'
+        })
+
+        .state('contributors', {
+          url: '/contributors',
+          templateUrl: 'partials/contributors.html',
+          controller: 'ContributorsCtrl'
+        })
+
+        .state('feedback', {
+          url: '/feedback',
+          templateUrl: 'partials/feedback.html',
+          controller: 'FeedbackFormCtrl'
+        })
+
+        .state('help', {
+          url: '/help',
+          templateUrl: 'partials/help.html',
+          controller: 'SearchHelpCtrl'
+        })
+
+        .state('faqs', {
+          url: '/faqs',
+          templateUrl: 'partials/faqs.html',
+          controller: 'FaqsCtrl'
+        });
+
+        // For nicer URLs w/out '#'. Note: <base> tag required on index.html with html5Mode
+        $locationProvider.html5Mode(true);
+
+    }]);
+
+})();
