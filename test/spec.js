87bc677bf0022369d27b22221d1b8a2852482542
Susan Ley <sley@getty.edu>
2015-11-19 21:53:45 +0000

    tests in progress, need to reconfigure Selenium, recheck for module dependencies, add for testing in multiple browsers

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,36 @@
 // spec.js
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+describe('Portal', function() {
+  
+  var searchBtn = element(by.id('go-btn'));
+  var seeAllBtn = element(by.id('see-all-button'));
+  var testQuery = 'paintings';
+  var bookListing = element.all(by.class('li.book-listing'));
+  var resultsCount = element.all(by.class('li.book-listing')).count();
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+  beforeEach(function() {
+    browser.get('http://local.portal.dev:8000');
   });
+
+  it('should return correct search results', function() {
+    browser.get('http://local.portal.dev:8000/search');
+    element(by.model('queryTerm')).sendKeys(testQuery);
+    searchBtn.click();
+
+    expect(Results.hits.hits.count()).toEqual(resultsCount);
+  });
+
+  it('should return all search results', function() {
+    browser.get('http://local.portal.dev:8000');
+    seeAllBtn.click();
+
+    expect(browser.getCurrentUrl()).toEqual('http://local.portal.dev:8000/search');
+  });
+
+  it('should return an item page', function() {
+    browser.get('http://local.portal.dev:8000/search?q=' + testQuery);
+    bookListing.element(by.css('.ng-binding a')).click();
+
+    expect($('[ng-repeat=key, field in book._source]').toBeTruthy());
+  });
+
 });
