5208b5e9052cf08b7c7dcf2b56b84c7bdae86f5b
adamcee <adam.cahan@gmail.com>
2015-12-16 19:09:29 -0800

    applying es mapping. changed date.value mapping to string.

--- scripts/init-es.sh
+++ scripts/init-es.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #Initialize ES inside the docker container: make index, add sample data, etc, start ES.
 
-echo "Starting elasticsearch..."
+printf "Starting elasticsearch...\n"
 /etc/init.d/elasticsearch start
-echo "Waiting for elasticsearch to get itself together...."
+printf "\n Waiting for elasticsearch to get itself together....\n"
 sleep 10
-echo "Creating portal index..."
+printf "Deleting existing index and data...\n"
+curl -XDELETE http://localhost:9200/portal
+sleep 2
+printf "\n Creating portal index...\n"
 curl -XPUT http://localhost:9200/portal
 sleep 2
-echo "Uploading sample data..."
+printf "\n Creating book mapping...\n"
+curl -XPUT http://localhost:9200/portal/_mapping/book -d @es_mapping_book.json
+sleep 2
+printf "\n Uploading sample data...\n"
 curl -s -XPOST http://localhost:9200/_bulk --data-binary @mocks/gri_marc21_batch
+printf "\n"
