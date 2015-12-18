e6b265441510ee9652d619545bc3f01f2a457b2d
adamcee <adam.cahan@gmail.com>
2015-12-18 13:47:09 -0800

    Fix merge conflicts for refactor-docker branch.

---
+++ init-es.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Initialize ES from outside the docker container: make index, add sample data, etc, start ES.
+
+echo "Waiting for elasticsearch to get itself together...."
+sleep 5
+printf "Deleting existing index and data...\n"
+curl -XDELETE http://local.portal.dev:9200/portal
+sleep 2
+echo "Creating portal index..."
+curl -XPUT http://local.portal.dev:9200/portal
+sleep 2
+printf "\n Creating book mapping...\n"
+curl -XPUT http://local.portal.dev:9200/portal/_mapping/book -d @es_mapping_book.json
+sleep 2
+echo "Uploading sample data..."
+curl -s -XPOST http://local.portal.dev:9200/_bulk --data-binary @mocks/gri_marc21_batch
