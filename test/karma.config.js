1846e3c5113271e63c7a1bf906c3499b2ff2edf4
adamcee <adam.cahan@gmail.com>
2016-02-01 15:50:26 -0800

    Update required files for testing in karma.config.js

--- test/karma.config.js
+++ test/karma.config.js
@@ -1,16 +1,24 @@
 module.exports = function(config){
   config.set({
 
-    basePath : '../',
+    basePath : '',
 
     files : [
+      // load angular app dependencies from bower
+      'app/bower_components/jquery/dist/jquery.js',
+      'app/bower_components/lodash/lodash.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/js/**/*.js',
-      'test/unit/**/*.js'
+      'app/bower_components/elasticsearch/elasticsearch.js',
+      'app/bower_components/angularUtils-pagination/dirPagination.js',
+      'app/bower_components/angular-print/angularPrint.js',
+
+      // app source code & test files
+      'app/*.js',
+      'app/**/*.js',
     ],
 
     autoWatch : true,
