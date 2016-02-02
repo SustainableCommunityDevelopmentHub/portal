e3e2d4bd5251a5af8084b64b740f42bf067938f9
Susan Ley <SLey@getty.edu>
2016-02-01 18:11:27 -0800

    Refactor complete

--- test/e2e/book-detail.spec.js
+++ test/e2e/book-detail.spec.js
@@ -1,29 +1,30 @@
 // book-detail.spec.js
 describe('Book Detail', function() {
 
-  var exportBtn = element(by.id('exportBtn'));
-  var testData = JSON.stringify(require('./book.json'));
+  
   var seeAllBtn = element(by.id('see-all-btn'));
   var firstResult = element.all(by.css('.portal-record-link')).first();
-
-
-  it('should return an item page', function() {
+  var exportBtn = element(by.id('exportBtn'));
+  var testData = JSON.stringify(require('./book.json'));
+    
+  
+  beforeEach(function() {
     browser.get('');
     seeAllBtn.click();
     browser.wait(function() {
       return firstResult.isPresent();
-    }, 2000);
+    }, 1000);
     var bookId;
-    element(by.css('.portal-record-link')).evaluate('book._id').then(function(value) {
+    firstResult.evaluate('book._id').then(function(value) {
       bookId = value;
-      console.log(bookId);
       var itemUrl = 'books/' + bookId;
       browser.get(itemUrl);
     });
+  });
+
+  it('should return an item page', function() {
     element.all(by.repeater('(key, field) in book._source.dublin_core')).then(function(posts) {
-      console.log(posts);
       expect(posts.length).toEqual(12);
-      console.log(posts);
       var titleElement = posts[11].$('.book-field-val');
       var dateElement = posts[3].$('.book-field-val');
       expect(titleElement.getText()).toEqual('La Chronique des arts et de la curiosité : supplément à la Gazette des beaux-arts');
@@ -32,22 +33,11 @@ describe('Book Detail', function() {
   });
 
   it('should download record in JSON format on click', function() {
-    browser.get('');
-    seeAllBtn.click();
-    browser.wait(function() {
-      return firstResult.isPresent();
-    }, 2000);
-    var bookId;
-    element(by.css('.portal-record-link')).evaluate('book._id').then(function(value) {
-      bookId = value;
-      console.log(bookId);
-      var itemUrl = 'books/' + bookId;
-      browser.get(itemUrl);
-    });
-    var exportBtn = element(by.id('exportBtn'));
     exportBtn.click();
     $('.saveJson').click();
     var fileContents = $('.saveJson').evaluate('fileContents');
     expect(testData).toEqual(fileContents);
   });
+
+  
 });
