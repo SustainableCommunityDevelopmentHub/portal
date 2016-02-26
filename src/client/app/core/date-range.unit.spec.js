81c619946ef3f799aa741df6e98ef78d612b4ed2
Susan Ley <sley@getty.edu>
2016-02-26 12:27:24 -0800

    Tests passing

---
+++ src/client/app/core/date-range.unit.spec.js
@@ -0,0 +1,116 @@
+describe("Date Range Filter", function() {
+  var scope, controller, $state, MySearchService, data, es, opts;
+
+  var baseESQuery = {
+    "index":"portal",
+    "type":"book",
+    "size":25,
+    "from":"0",
+    "body":{
+      "query":{
+        "filtered":{
+          "query":{
+            "match_all":{}
+          },
+          "filter": {
+            "bool": {
+              "must": [],
+              "filter": []
+            }
+          }
+        }
+      },
+      "aggregations":{
+        "creator":{
+            "terms":{
+              "field":"_creator_facet.raw"
+            }
+          },
+          "language":{
+            "terms":{
+              "field":"_language"
+            }
+          },
+          "grp_contributor":{
+            "terms":{
+              "field":"_grp_contributor.raw"
+            }
+          },
+          "subject":{
+            "terms":{
+              "field":"_subject_facets.raw"
+            }
+          },
+          "type":{
+            "terms":{
+              "field":"_grp_type.raw"
+            }
+          }
+        }
+      }
+    
+  };
+
+  beforeEach(function(){
+    module('ui.router');
+    module('ui.bootstrap');
+    module('elasticsearch');
+    module('app.core');
+    module('app');
+    module('app.search');
+  });
+  
+  beforeEach(inject(function($rootScope, $controller, _$state_, SearchService, DataService, esClient){
+    data = DataService;
+    es = esClient;
+    $state = _$state_;
+    scope = $rootScope.$new();
+    searchService = SearchService;
+
+    controller = $controller('SearchCtrl', {
+        '$scope': scope,
+        '$state': $state,
+        'SearchService': searchService
+      });
+    }));
+
+  it("should call Search Service's update opts when calling setDateRange", function(){
+    spyOn(searchService, 'updateOpts');
+    scope.setDateRange(scope.fromDate, scope.toDate);
+    expect(searchService.updateOpts).toHaveBeenCalled();
+  });
+
+  it("adds date range object to SearchService's options", function(){
+    scope.setDateRange(scope.fromDate, scope.toDate);
+    expect(searchService.opts.date).toBeDefined();
+    expect(searchService.opts.date.gte).toEqual(scope.fromDate);
+    expect(searchService.opts.date.lte).toEqual(scope.toDate);
+  });
+
+  describe("Tests for building elasticsearch date range query", function(){
+    beforeEach(function(){
+      opts = {"facets":[],"page":1,"from":'0', size: 25};
+      spyOn(es, 'search');
+
+    });
+
+    afterEach(function(){
+      /* Tear down properly so state does not persist between tests */
+      delete opts.date;
+      if(baseESQuery.body.date){
+        delete baseESQuery.body.date;   
+      }
+    });
+
+    it("builds correct elasticsearch query for date range", function(){
+      opts.date = {"gte": scope.fromDate,"lte": scope.toDate};
+      var dateRangeQuery = baseESQuery;
+      dateRangeQuery.body.query.filtered.filter.bool.must = [{"range":{"_date_facet":{"gte": scope.fromDate,"lte": scope.toDate}}}];
+      data.search(opts);
+      expect(es.search).toHaveBeenCalledWith(dateRangeQuery);    
+    });
+
+
+  });
+
+});
