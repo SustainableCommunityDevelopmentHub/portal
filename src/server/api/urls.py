c2ab92ce22f8cb7d692188a89ccf8ab7ac8d448d
JFRANCO <jfranco@getty.edu>
2016-05-12 17:53:17 -0700

    Added Books APIView for search queries. Rewrote query-builder and got search queries working

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from api.views import Book, Contributors
+from api.views import Book, Books, Contributors
 
 urlpatterns = [
     url(r'^book/(?P<id>\w+)[/]?$', Book.as_view(), name='book'),
+    url(r'^books/(?P<params>[^.]*)$', Books.as_view(), name='books'),
     url(r'^contributors[/]?$', Contributors.as_view()),
     #url(r'^book/', Book.as_view()),
 ]
--- src/server/api/views.py
+++ src/server/api/views.py
@@ -10,6 +10,8 @@ from rest_framework import status
 from rest_framework import renderers
 
 import json
+import urllib
+from urllib.parse import urlparse
 
 class Book(APIView):
     def get(self, request, id, format=None):
@@ -28,3 +30,204 @@ class Contributors(APIView):
         response = s.execute()
         response_json = json.loads(json.dumps(response.to_dict()))
         return Response(response_json['aggregations']['grp_contributor']['buckets'], status=status.HTTP_200_OK)
+
+class Books(APIView):
+    def get(self, request, params, format=None):
+        search_options = urllib.parse.parse_qs(params)
+        es = Elasticsearch(['local.portal.dev:9200'])
+        body = self.create_base_query()
+        filters = []
+        advanced_filters = []
+        body['query']['bool']['must'] = self.create_query_string(search_options.get('q'))
+
+        body['sort'] = self.create_sort_query(search_options.get('sort'))
+
+        if search_options.get('date_gte') or search_options.get('date_lte'):
+            date_query = self.create_date_query(search_options.get('date_gte'), search_options.get('date_lte'))
+            print(date_query)
+            body['query']['bool']['filter']['bool']['filter'].append(date_query)
+            filters.append(date_query)
+
+        advanced_fields = ['adv_date','adv_creator', 'adv_subject', 'adv_title', 'adv_contributor', 'adv_language']
+        for field in advanced_fields:
+            if search_options.get(field):
+                adv_filters = self.create_advanced_filters(field, search_options.get(field))
+                for filter in adv_filters:
+                    body['query']['bool']['filter']['bool']['filter'].append(filter)
+                    advanced_filters.append(filter)
+
+        if len(advanced_filters):
+            filters.append(advanced_filters)
+
+        facet_categories = ['creator', 'subject', 'grp_contributor', 'language']
+        for category in facet_categories:
+            if search_options.get(category):
+                facet_filters = self.create_facet_filters(category, search_options.get(category))
+                body['query']['bool']['filter']['bool']['must'].append(facet_filters)
+                for other_category in facet_categories:
+                    if other_category != category:
+                        body['aggregations'][other_category]['filter']['bool']['must'].append(facet_filters)
+
+        query = self.create_multisearch(body, search_options.get('start'), search_options.get('size'), filters)
+        response = es.msearch(body=query)
+        j = json.loads(json.dumps(response))
+        return Response(j['responses'], status=status.HTTP_200_OK)
+
+    def create_sort_query(self, sort):
+        valid_sorts = {'date_added': {"_ingest_date": {"order": "desc"}},
+                       'title_asc': "_title_display.sort",
+                       "title_desc": {"_title_display.sort": {"order": "desc"}},
+                       "date_asc": "_date_facet",
+                       "date_desc": {"_date_facet": {"order": "desc"}}}
+        if sort:
+            return valid_sorts.get(sort[0], [])
+        else:
+            return []
+
+    def create_date_query(self, from_date, to_date):
+        if from_date:
+            date_gte = from_date[0]
+        else:
+            date_gte = None
+        if to_date:
+            date_lte = to_date[0]
+        else:
+            date_lte = None
+        return {"range": {"_date_facet": {"gte": date_gte,
+                                          "lte": date_lte}}}
+
+    def create_facet_filters(self, category, facets):
+        filters = []
+        keys = {'creator': '_creator_facet.raw',
+                'subject': '_subject_facets.raw',
+                'grp_contributor': '_grp_contributor.raw',
+                'language': '_language'}
+        key = keys.get(category)
+        if key:
+            filters = [{'term': {key: facet}} for facet in facets]
+        return {'bool': {'should': filters}}
+
+    def create_advanced_filters(self, field, terms):
+        fields_term = []
+        if field == 'adv_contributor':
+            fields_term = ['_grp_contributor', '_grp_contributor.folded']
+        elif field == 'adv_date':
+            fields_term = ['_date_facet.folded', 'dublin_core.date.value', 'dublin_core.date.value.folded']
+        elif field == 'adv_creator':
+            fields_term = ['dublin_core.creator.value', 'dublin_core.creator.value.folded']
+        elif field == 'adv_subject':
+            fields_term = ['dublin_core.subject.value', 'dublin_core.subject.value.folded']
+        elif field == 'adv_language':
+            fields_term = ['dublin_core.language.value', 'dublin_core.language.value.folded']
+        elif field == 'adv_title':
+            fields_term = ['dublin_core.title.value', 'dublin_core.title.value.folded']
+
+        filters = []
+        for term in terms:
+            filter = {'query_string': {'query': term,
+                                       'minimum_should_match': '2<-1 5<75%',
+                                       'fields': fields_term}}
+            filters.append(filter)
+        return filters
+
+    def create_base_query(self):
+        query = {"query": {"bool": {"must": {},
+                                    "filter": {"bool": {"must": [],
+                                                        "filter": []}}}},
+                 "aggregations": {"creator": {"filter": {'bool': {'must': []}},
+                                              "aggs": {"creator":
+                                                        {"terms": {"field": "_creator_facet.raw", "size": 1000}}}},
+                                  "language": {"filter": {'bool': {'must': []}},
+                                               "aggs": {"language":
+                                                        {"terms": {"field": "_language", "size": 1000}}}},
+                                  "grp_contributor": {"filter": {'bool': {'must': []}},
+                                                      "aggs": {"grp_contributor":
+                                                                {"terms": {"field": "_grp_contributor.raw", "size": 1000}}}},
+                                  "subject": {"filter": {'bool': {'must': []}},
+                                              "aggs": {"subject":
+                                                        {"terms": {"field": "_subject_facets.raw", "size": 1000}}}}}}
+        return query
+
+    def create_query_string(self, q):
+        if q:
+            return {'query_string': {'query': q[0],
+                                     'minimum_should_match': '2<-1 5<75%',
+                                     'fields': ['_record_link',
+                                                '_language',
+                                                '_language.folded',
+                                                '_grp_type',
+                                                '_title_display^18',
+                                                '_title_display.folded^18',
+                                                '_subject_facets^15',
+                                                '_subject_facets.folded^15',
+                                                '_creator_display',
+                                                '_creator_display.folded',
+                                                '_creator_facet^12',
+                                                '_creator_facet.folded^12',
+                                                '_date_facet.folded',
+                                                '_date_display',
+                                                '_date_display.folded',
+                                                '_grp_contributor',
+                                                '_grp_contributor.folded',
+                                                '_grp_id',
+                                                '_edition',
+                                                '_edition.folded',
+                                                '_series',
+                                                '_series.folded',
+                                                'dublin_core.identifier.value',
+                                                'dublin_core.identifier.value.folded',
+                                                'dublin_core.creator.value',
+                                                'dublin_core.creator.value.folded',
+                                                'dublin_core.date.value',
+                                                'dublin_core.date.value.folded',
+                                                'dublin_core.publisher.value',
+                                                'dublin_core.publisher.value.folded',
+                                                'dublin_core.format.value',
+                                                'dublin_core.format.value.folded',
+                                                'dublin_core.type.value',
+                                                'dublin_core.type.value.folded',
+                                                'dublin_core.description.value',
+                                                'dublin_core.description.value.folded',
+                                                'dublin_core.provenance.value',
+                                                'dublin_core.provenance.value.folded',
+                                                'dublin_core.language.value',
+                                                'dublin_core.language.value.folded',
+                                                'dublin_core.subject.value',
+                                                'dublin_core.subject.value.folded',
+                                                'dublin_core.coverage.value',
+                                                'dublin_core.coverage.value.folded',
+                                                'dublin_core.title.value',
+                                                'dublin_core.title.value.folded',
+                                                'dublin_core.contributor.value',
+                                                'dublin_core.contributor.value.folded',
+                                                'dublin_core.relation.value',
+                                                'dublin_core.relation.value.folded',
+                                                'dublin_core.source.value',
+                                                'dublin_core.source.value.folded',
+                                                'dublin_core.rights.value',
+                                                'dublin_core.rights.value.folded',
+                                                'dublin_core.accrualMethod.value',
+                                                'dublin_core.accrualMethod.value.folded',
+                                                'dublin_core.accrualPeriodicity.value',
+                                                'dublin_core.accrualPeriodicity.value.folded',
+                                                'dublin_core.audience.value',
+                                                'dublin_core.audience.value.folded']}}
+        else:
+            return {'match_all': {}}
+
+    def create_multisearch(self, body, start, size, filters):
+        index = {'index': 'portal', 'doc_type': 'book'}
+        query = [index,
+                 {'size': size[0],
+                  'from': start[0],
+                  'query': body.get('query'),
+                  'sort': body.get('sort')
+                },
+                 index,
+                 {'size': 0,
+                  'query': {'bool': {'must': body['query']['bool']['must'],
+                                     'filter': {'bool': {'must': filters}}}},
+                  'aggregations': body['aggregations']}]
+
+        request = [json.dumps(item) for item in query]
+        return ' \n'.join(request)
