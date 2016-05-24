3f026f92b1c49493a032c835a75f654e2fee46e5
Susan Ley <sley@getty.edu>
2016-05-24 16:52:56 -0700

    Added links and tests

--- test/e2e/search-home.e2e.spec.js
+++ test/e2e/search-home.e2e.spec.js
@@ -26,13 +26,23 @@ describe('Home Page', function() {
 		expect(totalTitles.getText()).toEqual('452');
 	});
 
-  it('Should display all records when user clicks See All button', function() {
+  	it('Should display all records when user clicks See All button', function() {
 		homePage.seeAll();
-    homePage.getHits().then(function(hits) {
-      expect(hits.length).toEqual(25);
-      expect(homePage.showingResultsDialogue).toEqual('Showing 1 - 25 of 452 results');
+    	homePage.getHits().then(function(hits) {
+      	expect(hits.length).toEqual(25);
+      	expect(homePage.showingResultsDialogue).toEqual('Showing 1 - 25 of 452 results');
     });
-  });
+
+  	it('Should have functioning footer links', function() {
+  		homePage.contactUs();
+  		browser.ignoreSynchronization = true;
+    	expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/about/contact_us.html');
+  		homePage.privacyPolicy();
+  		expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/legal/privacy.html');
+  		homePage.termsOfUse();
+  		expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/legal/copyright.html');
+  		browser.ignoreSynchronization = false;
+  	});
 
 });
 
