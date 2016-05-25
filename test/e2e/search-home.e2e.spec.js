f879ac29b714654f4f4a069acfb34c903020b836
Susan Ley <sley@getty.edu>
2016-05-24 17:31:48 -0700

    All tests passing

--- test/e2e/search-home.e2e.spec.js
+++ test/e2e/search-home.e2e.spec.js
@@ -29,20 +29,30 @@ describe('Home Page', function() {
   	it('Should display all records when user clicks See All button', function() {
 		homePage.seeAll();
     	homePage.getHits().then(function(hits) {
-      	expect(hits.length).toEqual(25);
-      	expect(homePage.showingResultsDialogue).toEqual('Showing 1 - 25 of 452 results');
+      		expect(hits.length).toEqual(25);
+      		expect(homePage.showingResultsDialogue).toEqual('Showing 1 - 25 of 452 results');
+      	});
     });
 
-  	it('Should have functioning footer links', function() {
+  	it('Should have functioning contact us link in footer', function() {
   		homePage.contactUs();
   		browser.ignoreSynchronization = true;
     	expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/about/contact_us.html');
+    	browser.ignoreSynchronization = false;
+    });
+
+    it('Should have functioning privacy policy link in footer', function() {
   		homePage.privacyPolicy();
+  		browser.ignoreSynchronization = true;
   		expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/legal/privacy.html');
-  		homePage.termsOfUse();
-  		expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/legal/copyright.html');
   		browser.ignoreSynchronization = false;
   	});
 
+	it('Should have functioning terms of use link in footer', function() {
+		homePage.termsOfUse();
+		browser.ignoreSynchronization = true;
+  		expect(browser.getCurrentUrl()).toContain('http://www.getty.edu/legal/copyright.html');
+  		browser.ignoreSynchronization = false;
+  	});
 });
 
