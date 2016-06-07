b6b285c8798ec6bf70355349fa8b13090788e373
alyxrossetti <arossetti@getty.edu>
2016-06-07 12:33:23 -0700

    Develop merged - still working out issues

--- src/server/api/urls.py
+++ src/server/api/urls.py
@@ -3,7 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api.views import Book, Books, Contributors
 
 urlpatterns = [
-<<<<<<< 542346e8dfab13bb3c0c7a5d965fcb0ffcf666e7
 	url(r'^book/(?P<id>\w+)\.raw$', Book.as_view(), name='book'),
     url(r'^book/(?P<id>(?:(?!.json|.xml|.api).)*)$', Book.as_view(), name='book'),
     # this matches urls with &,.='_- spaces and characters.
