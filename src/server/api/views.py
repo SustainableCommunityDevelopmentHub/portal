fd4000aefae72d2809ac8c538aafcc368c3be546
alyxrossetti <arossetti@getty.edu>
2016-06-07 20:58:45 -0700

    Exports working on server side. JSON export working correctly on Client side. Client side still needs to be pointed at RIS export.

--- src/server/api/views.py
+++ src/server/api/views.py
@@ -15,20 +15,26 @@ from api.transform import dc_export
 
 ELASTICSEARCH_ADDRESS = settings.ELASTICSEARCH_HOST + ":" + settings.ELASTICSEARCH_PORT
 
-class Book(APIView):
-    def get(self, request, id, raw=True, format=None):
+class Raw(APIView):
+    def get(self, request, id, format=None):
         es = Elasticsearch([ELASTICSEARCH_ADDRESS])
         book_id = id
         response = es.get(index='portal', doc_type='book', id=book_id, request_timeout=30)
-        if raw:
-            j = json.loads(json.dumps(response))
+        j = json.loads(json.dumps(response))
 
-            return Response(j, status=status.HTTP_200_OK)
-        else:
-            dc = dc_export(response)
-            j = json.loads(json.dumps(dc))
+        return Response(j, status=status.HTTP_200_OK)
 
-            return Response(j, status=status.HTTP_200_OK)
+
+class Book(APIView):
+    def get(self, request, id,  format=None):
+        es = Elasticsearch([ELASTICSEARCH_ADDRESS])
+        book_id = id
+        response = es.get(index='portal', doc_type='book', id=book_id, request_timeout=30)
+
+        dc = dc_export(response)
+        j = json.loads(json.dumps(dc))
+
+        return Response(j, status=status.HTTP_200_OK)
 
 
 class Contributors(APIView):
