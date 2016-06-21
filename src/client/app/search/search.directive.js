90c7d709b82c0d06d916fbfa7e42ae8e1bd98f39
JFRANCO <jfranco@getty.edu>
2016-06-21 16:18:04 -0700

    Fixed some bugs after testing with mobile emulators

--- src/client/app/search/search.directive.js
+++ src/client/app/search/search.directive.js
@@ -19,12 +19,6 @@
       elem[0].querySelector('.search-input').focus();
     };
   })
-  .directive('focusSearchOnLoad', function() {
-    return function(scope, elem, attr){
-      elem[0].querySelector('#facet-chip-input').focus();
-      elem.addClass('input-div-focus');
-    };
-  })
   .directive('focusSearchInput', function(){
     return function(scope, elem, attr){
 
@@ -37,15 +31,18 @@
       $(document).on("click", function(e) {
         if (!$(e.target).hasClass("facet-search") && !$(e.target).is("a") && !$(e.target).is("button") && e.target.type !== "checkbox") {
           if(elem.hasClass('input-div-focus')){
-            //console.log("removing focus");
             elem.removeClass('input-div-focus');
-          }          
+          }
         }
       });
-      elem.bind("click", function(){
-        var input = elem[0].querySelector('#facet-chip-input');
-        input.focus();
-        elem.addClass('input-div-focus');
+      elem.bind("click", function(e){
+        var width = document.documentElement.clientWidth;
+        var target = angular.element(e.target);
+        if (width > 767 || target.hasClass('facet-chip-search-bar') || target.hasClass('facet-chip-list')) {
+          var input = elem[0].querySelector('#facet-chip-input');
+          input.focus();
+          elem.addClass('input-div-focus');
+        }
       });
     };
   });
