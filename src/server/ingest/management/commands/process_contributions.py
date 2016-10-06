482bd742fb5602556a01a9fdec0ea4624884cc3f
alyxrossetti <arossetti@getty.edu>
2016-10-06 13:14:47 -0700

    Created command and started reworking tasks.py without a class

---
+++ src/server/ingest/management/commands/process_contributions.py
@@ -0,0 +1,31 @@
+from glob import glob
+
+from  django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from ingest import tasks
+
+class Command(BaseCommand):
+	help = 'Creates source data from supplied data, adds source data to database, transforms data and pushes to ES'
+
+	def add_arguments(self, parser):
+		parser.add_argument('data_path', choices=['test', 'production'])
+		#parser.add_argument('es', choices=['local', 'dev', 'production'])
+		#parser.add_argument('metadata_type', choices=['marc', 'dc', 'mets'])
+		#parser.add_argument('in_dir', help='name of new supplied_data dir')
+		#parser.add_argument('-u', '--update', dest='update_es', action='store_true', help='reprocess data and push updated doc to ES')
+		parser.add_argument('-n', '--new', dest='new', action='store_true', help='process only new contributions')
+
+	def handle(self, *args, **options):
+		if options['data_path'] == 'test':
+			data_path = settings.TEST_DATA
+		elif options['data_path'] == 'production':
+			data_path = settings.PRODUCTION_DATA
+
+		if options['new'] is True:
+			supplied_dirs = '{}/supplied_data/*'.format(data_path)
+			for supplied_dir in glob(supplied_dirs):
+				tasks.create_source(data_path, supplied_dir)
+
+
+
--- src/server/ingest/tasks.py
+++ src/server/ingest/tasks.py
@@ -1,18 +1,53 @@
 import os
-import argparse
+import zipfile
+import shutil
 from glob import glob
-import json
 import requests
 import filecmp
 from datetime import datetime
 
+from django.db import models
 from django.conf import settings
 from pymarc import MARCWriter
 from lxml import etree
 
 from funnel import marc, dublin_core, mets
-from ingest.models import Record, Contributor
 from ingest import helpers
+from ingest.models import Contributor, Record
+
+
+
+MARC_DATA = [
+	'artic',
+	'avery',
+	'brooklyn',
+	'cleveland',
+	'frick',
+	'gpvl',
+	'gri',
+	'menil',
+	'ngcanada',
+	'osci',
+	'pma',
+	'smithsonian',
+	'warburg'
+]
+
+DC_DATA = [
+	'bnf',
+	'clark',
+	'inha',
+	'malaga',
+	'met',
+	'wolf'
+]
+
+METS_DATA = [
+	'hertz',
+	'hkhi',
+	'khi',
+	'uh'
+]
 
 SAMPLE_DATA = [
 	'bnf',
@@ -22,198 +57,107 @@ SAMPLE_DATA = [
 	'uh'
 ]
 
-class Contribution(object):
+def create_source(data_path, supplied_dir):
+	inst, idate, date_dir = assign_directories(data_path, supplied_dir)
+	if inst in MARC_DATA:
+		create_source_marc(data_path, supplied_dir, inst, idate, date_dir)
+	elif inst in DC_DATA:
+		create_source_dc(data_path, supplied_dir, inst, idate, date_dir)
+	elif inst in METS_DATA:
+		create_source_mets(data_path, supplied_dir, inst, idate, date_dir)
 
-	def __init__(self, data_path, metadata_type, in_dir, es, update_es=False, create_source=False):
-		if data_path == 'test':
-			self.data_path = settings.TEST_DATA
-		elif data_path == 'production':
-			self.data_path = settings.PRODUCTION_DATA
-		self.metadata_type = metadata_type
-		self.supplied_dir = '{}/supplied_data/{}_data/{}'.format(self.data_path, metadata_type, in_dir)
-		self.inst = in_dir.split('_')[0]
-		self.idate = in_dir.split('_')[-1]
-		self.inst_dir = os.path.join(self.data_path, 'source_data', self.inst)
-		if not os.path.isdir(self.inst_dir):
-			os.mkdir(self.inst_dir)
-		self.date_dir = os.path.join(self.inst_dir, self.idate)
-		if not os.path.isdir(self.date_dir):
-			os.mkdir(self.date_dir)
-		if es == 'local':
-			self.es = settings.LOCAL
-		elif es == 'dev': 
-			self.es = settings.DEV
-		elif es == 'production':
-			self.es = settings.PROD
-		self.update_es = update_es
-		self.create_source = create_source
+def assign_directories(data_path, supplied_dir):
+	inst = supplied_dir.split('/')[-1].split('_')[0]
+	idate = supplied_dir.split('/')[-1].split('_')[-1]
+	inst_dir = os.path.join(data_path, 'source_data', inst)
+	if not os.path.isdir(inst_dir):
+		os.mkdir(inst_dir)
+	date_dir = os.path.join(inst_dir, idate)
+	if not os.path.isdir(date_dir):
+		os.mkdir(date_dir)
+	return inst, idate, date_dir
 
-	def create_source_marc(self):
-		infs = '{}/*'.format(self.supplied_dir)
-		for f in glob(infs):
-			marc_list = helpers.get_marc_list(f)
-			for record in marc_list:
-				recid = helpers.get_marc_id(self.inst, record)
-				outname = '{}.mrc'.format(recid)
-				try:
-					with open(os.path.join(self.date_dir, outname), 'wb') as outf:
+def create_source_marc(data_path, supplied_dir, inst, idate, date_dir):
+	infs = '{}/*'.format(supplied_dir)
+	for f in glob(infs):
+		marc_list = helpers.get_marc_list(f)
+		for record in marc_list:
+			recid = helpers.get_marc_id(inst, record)
+			outname = '{}.mrc'.format(recid)
+			try:
+				with open(os.path.join(date_dir, outname), 'wb') as outf:
+					writer = MARCWriter(outf)
+					writer.write(record)
+				print('Created source record {}'.format(os.path.join(date_dir, outname)))
+			except UnicodeEncodeError:
+				if inst == 'gri' and record.leader[9] == ' ':
+					record.leader = record.leader[0:9] + 'a' + record.leader[10:]
+					with open(os.path.join(date_dir, outname), 'wb') as outf:
 						writer = MARCWriter(outf)
 						writer.write(record)
-					print('Created source record {}'.format(os.path.join(self.date_dir, outname)))
-				except UnicodeEncodeError:
-					if self.inst == 'gri' and record.leader[9] == ' ':
-						record.leader = record.leader[0:9] + 'a' + record.leader[10:]
-						with open(os.path.join(self.date_dir, outname), 'wb') as outf:
-							writer = MARCWriter(outf)
-							writer.write(record)
-						print('Created source record {}'.format(os.path.join(self.date_dir, outname)))
+					print('Created source record {}'.format(os.path.join(date_dir, outname)))
+	archive(data_path, inst, supplied_dir)
 
-	def create_source_dc(self):
-		infs = '{}/*'.format(self.supplied_dir)
-		for f in glob(infs):
-			with open(f, 'rb') as inf:
-				record = etree.parse(inf)
-				root = record.getroot()
-				nsmap = root.nsmap
-				if self.inst == 'met':
-					ident = helpers.get_met_id(record, root, nsmap)
-				elif self.inst == 'malaga':
-					ident = helpers.get_malaga_id(record, root, nsmap)
-				else:
-					ident = helpers.get_dc_id(self.inst, f, record, root, nsmap)
-			if ident is not None:
-				recid = '{}_{}'.format(self.inst, ident)
-				outname = '{}.xml'.format(recid)
-				with open(os.path.join(self.date_dir, outname), 'wb') as outf:
-					record.write(outf, encoding='UTF-8')
-				print('Created source record {}'.format(os.path.join(self.date_dir, outname)))
-
-	def create_source_mets(self):
-		infs = '{}/*'.format(self.supplied_dir)
-		for f in glob(infs):
-			with open(f, 'r') as inf:
-				record = helpers.get_mets_rec(self.inst, inf)
-				root = record.getroot()
-				nsmap = root.nsmap
-				if self.inst == 'hertz':
-					ident = helpers.get_mets_id(record, root, nsmap, 'METS')
-				elif self.inst == 'hkhi':
-					ident = helpers.get_hkhi_id(record, root, nsmap)
-				elif self.inst == 'khi':
-					ident = helpers.get_khi_id(record, root, nsmap)
-				elif self.inst.startswith('uh'):
-					ident = helpers.get_mets_id(record, root, nsmap, 'mets')
-				if ident is not None:
-					if self.inst == 'hkhi':
-						recid = 'khi_{}'.format(ident)
-					else:
-						recid = '{}_{}'.format(self.inst, ident)
-					outname = '{}.xml'.format(recid)
-					with open(os.path.join(self.date_dir, outname), 'wb') as outf:
-						record.write(outf, encoding='UTF-8')
-					print('Created source record {}'.format(os.path.join(self.date_dir, outname)))
-
-	def process_data(self):	
-		fpaths = '{}/*'.format(self.date_dir)
-		for fpath in glob(fpaths):
-			recid = fpath.split('/')[-1].split('.')[0]
-			erec = Record.objects.filter(pk=recid)
-			if len(erec) == 0:
-				self.process_new_rec(recid, fpath)
-			elif len(erec) == 1:
-				oldrec = erec[0]
-				if oldrec.updated_date:
-					old_date = oldrec.updated_date
-				else:
-					old_date = oldrec.ingest_date
-				idate = datetime.strptime(self.idate, '%Y-%m-%d').date()
-				if idate >= old_date:
-					self.process_dupe_rec(oldrec, recid, fpath)
-
-	def process_new_rec(self, recid, fpath):
-		contrib = Contributor.objects.get(pk=self.inst)
-		if self.metadata_type == 'marc':
-			Record.objects.create(pk=recid, ingest_date=self.idate, contributor=contrib, source_path=fpath, source_schema='MA')
-			rec = marc.main(fpath)
-		elif self.metadata_type == 'dc':
-			Record.objects.create(pk=recid, ingest_date=self.idate, contributor=contrib, source_path=fpath, source_schema='DC')
-			rec = dublin_core.main(fpath)
-		elif self.metadata_type == 'mets':
-			Record.objects.create(pk=recid, ingest_date=self.idate, contributor=contrib, source_path=fpath, source_schema='ME')
-			rec = mets.main(fpath)
-		if self.es == 'http://local.portal.dev:9200':
-			if self.inst == 'gri' and self.idate == '2015-10-19':
-				self.load_es(recid, rec)
-			elif self.inst in SAMPLE_DATA:
-				self.load_es(recid, rec)
-		else:
-			self.load_es(recid, rec)
-
-	def process_dupe_rec(self, oldrec, recid, fpath):
-		print('Duplicate Record Found')
-		oldpath = oldrec.source_path
-		diff = filecmp.cmp(fpath, oldpath, shallow=False)
-		if diff is False:
-			print('Update Record in DB\n')
-			oldrec.updated_date = self.idate
-			oldrec.source_path = fpath
-			oldrec.save()
-		if diff is False or self.update_es is True:
-			print('Update Record in ES\n')
-			if self.metadata_type == 'marc':
-				rec = marc.main(fpath)
-			elif self.metadata_type == 'dc':
-				rec = dublin_core.main(fpath)
-			elif self.metadata_type == 'mets':
-				rec = mets.main(fpath)
-			if self.es == 'http://local.portal.dev:9200':
-				if self.inst == 'gri' and self.idate == '2015-10-19':
-					self.load_es(recid, rec)
-				elif self.inst in SAMPLE_DATA:
-					self.load_es(recid, rec)
+def create_source_dc(data_path, supplied_dir, inst, idate, date_dir):
+	infs = '{}/*'.format(supplied_dir)
+	for f in glob(infs):
+		with open(f, 'rb') as inf:
+			record = etree.parse(inf)
+			root = record.getroot()
+			nsmap = root.nsmap
+			if inst == 'met':
+				ident = helpers.get_met_id(record, root, nsmap)
+			elif inst == 'malaga':
+				ident = helpers.get_malaga_id(record, root, nsmap)
 			else:
-				self.load_es(recid, rec)
-		if diff is True and self.update_es is False:
-			print('No Diff, No ES Update, Do Nothing\n')
+				ident = helpers.get_dc_id(inst, f, record, root, nsmap)
+		if ident is not None:
+			recid = '{}_{}'.format(inst, ident)
+			outname = '{}.xml'.format(recid)
+			with open(os.path.join(date_dir, outname), 'wb') as outf:
+				record.write(outf, encoding='UTF-8')
+			print('Created source record {}'.format(os.path.join(date_dir, outname)))
+	archive(data_path, inst, supplied_dir)
 
 
-	def load_es(self, recid, rec):
-		ES_DOC = '{}/portal/book/{}'.format(self.es, recid)
+def create_source_mets(data_path, supplied_dir, inst, idate, date_dir):
+	infs = '{}/*'.format(supplied_dir)
+	for f in glob(infs):
+		with open(f, 'r') as inf:
+			record = helpers.get_mets_rec(inst, inf)
+			root = record.getroot()
+			nsmap = root.nsmap
+			if inst == 'hertz':
+				ident = helpers.get_mets_id(record, root, nsmap, 'METS')
+			elif inst == 'hkhi':
+				ident = helpers.get_hkhi_id(record, root, nsmap)
+			elif inst == 'khi':
+				ident = helpers.get_khi_id(record, root, nsmap)
+			elif inst.startswith('uh'):
+				ident = helpers.get_mets_id(record, root, nsmap, 'mets')
+			if ident is not None:
+				if inst == 'hkhi':
+					recid = 'khi_{}'.format(ident)
+				else:
+					recid = '{}_{}'.format(inst, ident)
+				outname = '{}.xml'.format(recid)
+				with open(os.path.join(date_dir, outname), 'wb') as outf:
+					record.write(outf, encoding='UTF-8')
+				print('Created source record {}'.format(os.path.join(date_dir, outname)))
+	archive(data_path, inst, supplied_dir)
 
-		try:
-			resp = requests.put(ES_DOC, data=rec.encode('utf-8'))
-		except:
-			print(recid)
-			raise
-		print('Uploading {}...{}\n'.format(recid, resp.status_code))
-
-	def contribute(self):
-		if self.create_source  is True:
-			if self.metadata_type == 'marc':
-				self.create_source_marc()
-			elif self.metadata_type == 'dc':
-				self.create_source_dc()
-			elif self.metadata_type == 'mets':
-				self.create_source_mets()
-
-		self.process_data()
-
-def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('data_path', choices=['test', 'production'])
-	parser.add_argument('es', choices=['local', 'dev', 'production'])
-	parser.add_argument('metadata_type', choices=['marc', 'dc', 'mets'])
-	parser.add_argument('in_dir', help='name of new supplied_data dir')
-	parser.add_argument('-u', '--update', dest='update_es', action='store_true', help='reprocess data and push updated doc to ES')
-	parser.add_argument('-c', '--create', dest='create_source', action='store_true', help='create source records from supplied data')
-
-	args = parser.parse_args()
-
-	contribution = Contribution(**vars(args))
-	contribution.contribute()
-
-	
+def archive(data_path, inst, supplied_dir):
+	archive_dir = os.path.join(data_path, 'archived_data', inst)
+	if not os.path.isdir(archive_dir):
+		os.mkdir(archive_dir)
+	zipname = supplied_dir.split('/')[-1]
+	zipname = '{}/{}.zip'.format(archive_dir, zipname)
+	with zipfile.ZipFile(zipname, 'w') as contrib_zip:
+		for dirname, subdirs, files in os.walk(supplied_dir):
+			for filename in files:
+				contrib_zip.write(os.path.join(dirname, filename), os.path.basename(filename))
+	shutil.rmtree(supplied_dir)
 
 
-if __name__ == '__main__':
-	main()
+
+
