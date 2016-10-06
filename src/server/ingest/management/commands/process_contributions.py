8daa58ce4d4d6ec27ea6a3fe17beb04d2f391643
alyxrossetti <arossetti@getty.edu>
2016-10-06 15:47:42 -0700

    Finished refactoring using command and functional tasks.py

--- src/server/ingest/management/commands/process_contributions.py
+++ src/server/ingest/management/commands/process_contributions.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
 	def add_arguments(self, parser):
 		parser.add_argument('data_path', choices=['test', 'production'])
-		#parser.add_argument('es', choices=['local', 'dev', 'production'])
+		parser.add_argument('es', choices=['local', 'dev', 'production'])
 		#parser.add_argument('metadata_type', choices=['marc', 'dc', 'mets'])
 		#parser.add_argument('in_dir', help='name of new supplied_data dir')
 		#parser.add_argument('-u', '--update', dest='update_es', action='store_true', help='reprocess data and push updated doc to ES')
@@ -22,10 +22,25 @@ class Command(BaseCommand):
 		elif options['data_path'] == 'production':
 			data_path = settings.PRODUCTION_DATA
 
+		if options['es'] == 'local':
+			es = settings.LOCAL
+		elif options['es'] == 'dev': 
+			es = settings.DEV
+		elif options['es'] == 'production':
+			es = settings.PROD
+
 		if options['new'] is True:
 			supplied_dirs = '{}/supplied_data/*'.format(data_path)
 			for supplied_dir in glob(supplied_dirs):
-				tasks.create_source(data_path, supplied_dir)
-
+				tasks.create_source(data_path, supplied_dir, es)
+		else:
+			inst_dirs = '{}/source_data/*'.format(data_path)
+			for inst_dir in glob(inst_dirs):
+				inst = inst_dir.split('/')[-1]
+				print(inst)
+				date_dirs = '{}/*'.format(inst_dir)
+				for date_dir in glob(date_dirs):
+					print(date_dir)
+					tasks.process_data(inst, date_dir, es)
 
 
--- src/server/ingest/tasks.py
+++ src/server/ingest/tasks.py
@@ -57,14 +57,14 @@ SAMPLE_DATA = [
 	'uh'
 ]
 
-def create_source(data_path, supplied_dir):
+def create_source(data_path, supplied_dir, es):
 	inst, idate, date_dir = assign_directories(data_path, supplied_dir)
 	if inst in MARC_DATA:
-		create_source_marc(data_path, supplied_dir, inst, idate, date_dir)
+		create_source_marc(data_path, supplied_dir, inst, idate, date_dir, es)
 	elif inst in DC_DATA:
-		create_source_dc(data_path, supplied_dir, inst, idate, date_dir)
+		create_source_dc(data_path, supplied_dir, inst, idate, date_dir, es)
 	elif inst in METS_DATA:
-		create_source_mets(data_path, supplied_dir, inst, idate, date_dir)
+		create_source_mets(data_path, supplied_dir, inst, idate, date_dir, es)
 
 def assign_directories(data_path, supplied_dir):
 	inst = supplied_dir.split('/')[-1].split('_')[0]
@@ -77,7 +77,7 @@ def assign_directories(data_path, supplied_dir):
 		os.mkdir(date_dir)
 	return inst, idate, date_dir
 
-def create_source_marc(data_path, supplied_dir, inst, idate, date_dir):
+def create_source_marc(data_path, supplied_dir, inst, idate, date_dir, es):
 	infs = '{}/*'.format(supplied_dir)
 	for f in glob(infs):
 		marc_list = helpers.get_marc_list(f)
@@ -97,8 +97,9 @@ def create_source_marc(data_path, supplied_dir, inst, idate, date_dir):
 						writer.write(record)
 					print('Created source record {}'.format(os.path.join(date_dir, outname)))
 	archive(data_path, inst, supplied_dir)
+	process_data(inst, date_dir, es)
 
-def create_source_dc(data_path, supplied_dir, inst, idate, date_dir):
+def create_source_dc(data_path, supplied_dir, inst, idate, date_dir, es):
 	infs = '{}/*'.format(supplied_dir)
 	for f in glob(infs):
 		with open(f, 'rb') as inf:
@@ -118,9 +119,10 @@ def create_source_dc(data_path, supplied_dir, inst, idate, date_dir):
 				record.write(outf, encoding='UTF-8')
 			print('Created source record {}'.format(os.path.join(date_dir, outname)))
 	archive(data_path, inst, supplied_dir)
+	process_data(inst, date_dir, es)
 
 
-def create_source_mets(data_path, supplied_dir, inst, idate, date_dir):
+def create_source_mets(data_path, supplied_dir, inst, idate, date_dir, es):
 	infs = '{}/*'.format(supplied_dir)
 	for f in glob(infs):
 		with open(f, 'r') as inf:
@@ -145,6 +147,7 @@ def create_source_mets(data_path, supplied_dir, inst, idate, date_dir):
 					record.write(outf, encoding='UTF-8')
 				print('Created source record {}'.format(os.path.join(date_dir, outname)))
 	archive(data_path, inst, supplied_dir)
+	process_data(inst, date_dir, es)
 
 def archive(data_path, inst, supplied_dir):
 	archive_dir = os.path.join(data_path, 'archived_data', inst)
@@ -157,6 +160,79 @@ def archive(data_path, inst, supplied_dir):
 			for filename in files:
 				contrib_zip.write(os.path.join(dirname, filename), os.path.basename(filename))
 	shutil.rmtree(supplied_dir)
+
+def process_data(inst, date_dir, es):
+	idate = date_dir.split('/')[-1]
+	fpaths = '{}/*'.format(date_dir)
+	for fpath in glob(fpaths):
+		recid = fpath.split('/')[-1].split('.')[0]
+		erec = Record.objects.filter(pk=recid)
+		if len(erec) == 0:
+			process_new_rec(inst, recid, idate, fpath, es)
+		elif len(erec) == 1:
+			oldrec = erec[0]
+			if oldrec.updated_date:
+				old_date = oldrec.updated_date
+			else:
+				old_date = oldrec.ingest_date
+			idate_obj = datetime.strptime(idate, '%Y-%m-%d').date()
+			if idate_obj >= old_date:
+				process_dupe_rec(oldrec, inst, recid, idate, fpath, es)
+
+def process_new_rec(inst, recid, idate, fpath, es):
+	contrib = Contributor.objects.get(pk=inst)
+	if inst in MARC_DATA:
+		Record.objects.create(pk=recid, ingest_date=idate, contributor=contrib, source_path=fpath, source_schema='MA')
+		rec = marc.main(fpath)
+	elif inst in DC_DATA:
+		Record.objects.create(pk=recid, ingest_date=idate, contributor=contrib, source_path=fpath, source_schema='DC')
+		rec = dublin_core.main(fpath)
+	elif inst in METS_DATA:
+		Record.objects.create(pk=recid, ingest_date=idate, contributor=contrib, source_path=fpath, source_schema='ME')
+		rec = mets.main(fpath)
+	if es == 'http://local.portal.dev:9200':
+		if inst == 'gri' and idate == '2015-10-19':
+			load_es(recid, rec, es)
+		elif inst in SAMPLE_DATA:
+			load_es(recid, rec, es)
+	else:
+		load_es(recid, rec, es)
+
+def process_dupe_rec(oldrec, inst, recid, idate, fpath, es):
+	print('Duplicate Record Found')
+	oldpath = oldrec.source_path
+	diff = filecmp.cmp(fpath, oldpath, shallow=False)
+	if diff is False:
+		print('Update Record in DB\n')
+		oldrec.updated_date = idate
+		oldrec.source_path = fpath
+		oldrec.save()
+
+	print('Update Record in ES\n')
+	if inst in MARC_DATA:
+		rec = marc.main(fpath)
+	elif inst in DC_DATA:
+		rec = dublin_core.main(fpath)
+	elif inst in METS_DATA:
+		rec = mets.main(fpath)
+	if es == 'http://local.portal.dev:9200':
+		if inst == 'gri' and idate == '2015-10-19':
+			load_es(recid, rec, es)
+		elif inst in SAMPLE_DATA:
+			load_es(recid, rec, es)
+	else:
+		load_es(recid, rec, es)
+
+def load_es(recid, rec, es):
+	ES_DOC = '{}/portal/book/{}'.format(es, recid)
+
+	try:
+		resp = requests.put(ES_DOC, data=rec.encode('utf-8'))
+	except:
+		print(recid)
+		raise
+	print('Uploading {}...{}\n'.format(recid, resp.status_code))
+
 
 
 
